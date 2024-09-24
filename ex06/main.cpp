61edae5d8c924c99b56189404e255ed04faf51cb
mstrauss <matthias.strauss@proton.me>
2024-09-24 16:12:44 +0200

    finished ex06

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,11 +6,33 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:00:37 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/09/24 15:17:06 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/09/24 16:11:49 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "harl.hpp"
+
+enum Level
+{
+	DEBUG,
+	INFO,
+	WARNING,
+	ERROR,
+	UNKNOWN
+};
+
+Level getLevel(const std::string &level)
+{
+	if (level == "DEBUG")
+		return DEBUG;
+	if (level == "INFO")
+		return INFO;
+	if (level == "WARNING")
+		return WARNING;
+	if (level == "ERROR")
+		return ERROR;
+	return UNKNOWN;
+}
 
 int main(int ac, char **av)
 {
@@ -29,19 +51,34 @@ int main(int ac, char **av)
 	}
 
 	std::string lvl = av[1];
-
 	if (lvl.empty())
 	{
 		std::cout << "Invalid level: empty string" << std::endl;
 		return 1;
 	}
-	(void)av;
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
-	harl.complain("");
-	harl.complain("TEST");
 
+	switch (getLevel(lvl))
+	{
+	case 0:
+		std::cout << "[ DEBUG ]" << std::endl;
+		harl.complain("DEBUG");
+		std::cout << std::endl;
+	case 1:
+		std::cout << "[ INFO ]" << std::endl;
+		harl.complain("INFO");
+		std::cout << std::endl;
+	case 2:
+		std::cout << "[ WARNING ]" << std::endl;
+		harl.complain("WARNING");
+		std::cout << std::endl;
+	case 3:
+		std::cout << "[ ERROR ]" << std::endl;
+		harl.complain("ERROR");
+		std::cout << std::endl;
+		break;
+	default:
+		std::cout << "Invalid level: " << lvl << std::endl;
+		break;
+	}
 	return 0;
 }
